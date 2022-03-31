3a46ce673bd0dea9f1ee5ae8e8d85e8cd8850c9c
ZoeFan1995 <unique84060327@gmail.com>
2022-03-31 16:37:17 +0800

    feat:優化Ass5解法

--- week-2/js/Ass5.js
+++ week-2/js/Ass5.js
@@ -1,16 +1,14 @@
 const nums = [2, 11, 15, 2, 8, 1]
-const target = 10
+const target = 26
 
 function twoSum(nums, target) {
   let result = false
   for (let i = 0; i < nums.length; i++) {
-    let secNums = nums
-    for (let sec = i + 1; sec < secNums.length; sec++) {
-      if (nums[i] + secNums[sec] === target) {
-        console.log("Ass5->", i, sec);
-        result = true
-        return
-      }
+    let res = target - nums[i]
+    if (nums.includes(res)) {
+      console.log("Ass5->", i, nums.indexOf(res));
+      result = true
+      return 
     }
   }
   if (result === false) {
