5f695431dbcd56bad8eff966469caa5b2b01237d
ZoeFan1995 <unique84060327@gmail.com>
2022-04-03 10:54:38 +0800

    feat:完成Ass4 新增功能: 1.點擊Welcome Message,轉成Have a Good Time! 2.點擊hanburger menu,出現side menu, 點擊close btn則關閉side menu 3.點擊Call to Action,展示出更多content

--- week-2/js/index.js
+++ week-2/js/index.js
@@ -1,7 +1,32 @@
-window.onload = function() {
-  max(array2)
+window.onload = function () {
+  const h1Text = document.querySelector("h1")
+  const hamburger = document.querySelector(".icon")
+  const closeBtn = document.querySelector("#closeBtn")
+  const sideMenu = document.querySelector("#sideMenu")
+  const callAction = document.querySelector("button")
+
+  max(array1)
   calculate(args2)
   avg(data)
   twoSum(nums, target)
 
+  //監聽事件
+  h1Text.addEventListener("click", () => {
+    h1Text.textContent = "Have a Good Time!"
+  })
+  hamburger.addEventListener("click", () => {
+    sideMenu.style.display = "block"
+  })
+  closeBtn.addEventListener("click", () => {
+    sideMenu.style.display = "none"
+  })
+  callAction.addEventListener("click", () => {
+    const main = document.querySelector("#main")
+    const children = main.children.length
+    let template = "";
+    for (let i = children + 1; i < children + 5; i++) {
+      template += `<article><h4>Content Box ${i}</h4></article>`
+    }
+    main.insertAdjacentHTML("beforeend", template)
+  })
 }
