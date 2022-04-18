c12f1ffa6ef58054d3bc79b779d6b30d99628e0c
ZoeFan1995 <unique84060327@gmail.com>
2022-04-18 19:30:07 +0800

    feat:新增window.alert作法

--- week-4/js/Ass1.js
+++ week-4/js/Ass1.js
@@ -1,7 +1,9 @@
 delayedResult(2, 10, 3000, function(result) {
   console.log("Ass1 ->", result);
 })
-
+delayedResult(2, 10, 3000, function(result) {
+  window.alert(`Ass1 ->  ${result}`)
+})
 
 function delayedResult(n1, n2 , delayTime, callback) {
   const result = n1+n2;
